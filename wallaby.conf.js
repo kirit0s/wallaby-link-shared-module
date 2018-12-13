b08b21798ad19bff999bdc2d5a9d1250c2d8c67d
Simon McEnlly <simon.mcenlly@gmail.com>
2018-12-14 09:11:08 +1000

    Update wallaby.conf.js

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,9 +1,15 @@
 module.exports = () => ({
-  files: ['app/src/**/*.js', 'shared/src/**/*.js'],
+  files: ['app/src/**/*.js', 'shared/src/**/*.js', '!node_modules/shared/**'],
   tests: ['app/tests/**/*.test.js'],
   env: {
     type: 'node',
     runner: 'node',
   },
   testFramework: 'ava',
+  setup: function(wallaby) {
+    var path = require('path');
+    const moduleAlias = require('module-alias')
+
+    moduleAlias.addAlias('shared', path.join(wallaby.projectCacheDir, 'shared/src/app.js'));
+  }
 });
